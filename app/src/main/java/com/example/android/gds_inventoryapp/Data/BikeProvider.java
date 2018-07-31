5425f5e5a983eb2a02e9bd34de4e2dc2db82dec2
Alin-Gabriel Ionita <alan@hiddenltd.com>
2018-07-31 19:21:44 +0100

    feat(MainActivity): add ListView, Provider, CursorAdapter to populate the main view and specced out the query and insert methods in the provider

---
+++ app/src/main/java/com/example/android/gds_inventoryapp/Data/BikeProvider.java
@@ -0,0 +1,202 @@
+package com.example.android.gds_inventoryapp.Data;
+
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.example.android.gds_inventoryapp.Data.BikeContract.BikeEntry;
+
+public class BikeProvider extends ContentProvider {
+    // Tag for the log messages
+    public static final String LOG_TAG = BikeProvider.class.getSimpleName();
+    // URI matcher codes
+    private static final int BIKES = 100;
+    private static final int SINGLE_BIKE = 101;
+    // Match a content URL to a specific code
+    private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // Static initializer. This is run the first time anything is called from this class.
+    static {
+        uriMatcher.addURI(
+                BikeContract.CONTENT_AUTHORITY,
+                BikeContract.PATH_BIKES,
+                BIKES);
+        uriMatcher.addURI(
+                BikeContract.CONTENT_AUTHORITY,
+                BikeContract.PATH_BIKES + "/#",
+                SINGLE_BIKE);
+    }
+
+    BikeDbHelper bikeDbHelper;
+
+    @Override
+    public boolean onCreate() {
+        bikeDbHelper = new BikeDbHelper(getContext());
+        return true;
+    }
+
+    // Make a query for a given URI
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri,
+                        @Nullable String[] projection,
+                        @Nullable String selection,
+                        @Nullable String[] selectionArgs,
+                        @Nullable String sortOrder) {
+        // Get readable database
+        SQLiteDatabase database = bikeDbHelper.getReadableDatabase();
+
+        // This cursor will hold the result of the query
+        Cursor cursor;
+
+        // Figure out if the URI matcher can match the URI to a specific code
+        int match = uriMatcher.match(uri);
+        switch (match) {
+            case BIKES:
+                // For BIKES query the entire table
+                cursor = database.query(BikeEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            case SINGLE_BIKE:
+                // For the SINGLE_BIKE, get the ID from the URI.
+                selection = BikeEntry._ID + "=?";
+                selectionArgs = new String[]{String.valueOf(ContentUris.parseId(uri))};
+                cursor = database.query(BikeEntry.TABLE_NAME,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null,
+                        null,
+                        sortOrder);
+                break;
+            default:
+                throw new IllegalArgumentException("Cannot query unknown URI " + uri);
+        }
+
+        cursor.setNotificationUri(getContext().getContentResolver(), uri);
+        return cursor;
+    }
+
+    // Insert new data into the Provider given ContentValues
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues contentValues) {
+        final int match = uriMatcher.match(uri);
+        switch (match) {
+            case BIKES:
+                if (contentValues == null) {
+                    throw new IllegalArgumentException("ContentValues are null");
+                }
+                return insertBike(uri, contentValues);
+            default:
+                throw new IllegalArgumentException("Insertion is not supported for " + uri);
+        }
+    }
+
+    // Insert data into db given ContentValues
+    private Uri insertBike(Uri uri, ContentValues contentValues) {
+        // Get readable database
+        SQLiteDatabase database = bikeDbHelper.getReadableDatabase();
+
+        // Check that the make is not null
+        String make = contentValues.getAsString(BikeEntry.COLUMN_MAKE);
+        if (make == null) {
+            throw new IllegalArgumentException("Bike entries require a make");
+        }
+
+        // Check that the model is not null
+        String model = contentValues.getAsString(BikeEntry.COLUMN_MODEL);
+        if (model == null) {
+            throw new IllegalArgumentException("Bike entries require a model");
+        }
+
+        // Check that the type of bike is one the provided
+        int type = contentValues.getAsInteger(BikeEntry.COLUMN_TYPE);
+        switch (type) {
+            case BikeEntry.TYPE_UNKNOWN:
+            case BikeEntry.TYPE_CARGO:
+            case BikeEntry.TYPE_CITY:
+            case BikeEntry.TYPE_ELECTRIC:
+            case BikeEntry.TYPE_FIXED:
+            case BikeEntry.TYPE_FOLDING:
+            case BikeEntry.TYPE_GRAVEL_AND_CROSS:
+            case BikeEntry.TYPE_HYBRID:
+            case BikeEntry.TYPE_KIDS:
+            case BikeEntry.TYPE_MOUNTAIN:
+            case BikeEntry.TYPE_RECUMBENT:
+            case BikeEntry.TYPE_ROAD:
+            case BikeEntry.TYPE_TANDEM:
+            case BikeEntry.TYPE_TOURING:
+                Log.i("ContentValues/Gender: ", "Bike entry type was passed correctly!");
+                break;
+            default:
+                throw new IllegalArgumentException("Bike entries require one of the assigned bike types.");
+        }
+
+        // Check that the price is not a negative number
+        int price = contentValues.getAsInteger(BikeEntry.COLUMN_PRICE);
+        if (price < 0) {
+            throw new IllegalArgumentException("Bike entries require a price input");
+        }
+
+        // Check that the quantity is not a negative number
+        int quantity = contentValues.getAsInteger(BikeEntry.COLUMN_QUANTITY);
+        if (quantity < 0) {
+            throw new IllegalArgumentException("Bike entries require a quantity input");
+        }
+
+        // Check that the supplier is not null
+        String supplier = contentValues.getAsString(BikeEntry.COLUMN_SUPPLIER);
+        if (supplier == null) {
+            throw new IllegalArgumentException("Bike entries require a supplier");
+        }
+
+        // Check that the supplier phone number is not null
+        String supplierPhone = contentValues.getAsString(BikeEntry.COLUMN_SUPPLIER_PHONE);
+        if (supplierPhone == null) {
+            throw new IllegalArgumentException("Bike entries require a supplier number");
+        }
+
+        // This cursor will hold the result of the query
+        long id = database.insert(BikeEntry.TABLE_NAME, null, contentValues);
+
+        if (id == -1) {
+            Log.e(LOG_TAG, "Failed to insert row for " + uri);
+            return null;
+        }
+
+        getContext().getContentResolver().notifyChange(uri, null);
+
+        // Once we know the ID of the new row in the table,
+        // return the new URI with the ID appended to the end of it
+        return ContentUris.withAppendedId(uri, id);
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues contentValues, @Nullable String s, @Nullable String[] strings) {
+        return 0;
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        return null;
+    }
+}
--- app/src/main/java/com/example/android/gds_inventoryapp/MainActivity.java
+++ app/src/main/java/com/example/android/gds_inventoryapp/MainActivity.java
@@ -1,101 +1,44 @@
 package com.example.android.gds_inventoryapp;
 
 import android.content.ContentValues;
-import android.content.res.Resources;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import com.example.android.gds_inventoryapp.Data.BikeContract.BikeEntry;
+import com.example.android.gds_inventoryapp.Data.BikeCursorAdapter;
 import com.example.android.gds_inventoryapp.Data.BikeDbHelper;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
     private BikeDbHelper bikeDbHelper;
+    private static final int BIKE_LOADER = 0;
+    private BikeCursorAdapter bikeCursorAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bikeDbHelper = new BikeDbHelper(this);
-        queryDatabase();
+        // Create an instance of the cursor adaptor
+        bikeCursorAdapter = new BikeCursorAdapter(this, null);
+
+        // Find ListView and set the bikeCursorAdapter
+        ListView bikeList = findViewById(R.id.list);
+        bikeList.setEmptyView(findViewById(R.id.empty));
+        bikeList.setAdapter(bikeCursorAdapter);
+
+        // Initialise the loader
+        getSupportLoaderManager().initLoader(BIKE_LOADER, null, this);
+
     }
-
-    private void queryDatabase() {
-            BikeDbHelper dbHelper = new BikeDbHelper(this);
-
-            // Create and/or open a database to read from it
-            SQLiteDatabase db = dbHelper.getReadableDatabase();
-
-            // define the projections
-            String[] projection = {
-                    BikeEntry._ID,
-                    BikeEntry.COLUMN_MAKE,
-                    BikeEntry.COLUMN_MODEL,
-                    BikeEntry.COLUMN_PRICE,
-            };
-
-        // Perform this raw SQL query "SELECT * FROM bikes"
-        // to get a Cursor that contains all rows from the bikes table.
-            Cursor cursor = db.query(
-                    BikeEntry.TABLE_NAME,
-                    projection,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null
-            );
-
-        TextView displayView = findViewById(R.id.text_view_pet);
-
-            try {
-                int bikeCount = cursor.getCount();
-                Resources res = getResources();
-                String bikesFound = res.getQuantityString(
-                        R.plurals.numberOfBikeAvailable, bikeCount, bikeCount
-                );
-                displayView.setText(bikesFound);
-
-                displayView.append(
-                        " | " + BikeEntry._ID + " | "
-                                + BikeEntry.COLUMN_MAKE + " | "
-                                + BikeEntry.COLUMN_MODEL + " | "
-                                + BikeEntry.COLUMN_PRICE + " | "
-                        + "\n");
-
-                // Find column indices
-                int idColumnIndex = cursor.getColumnIndex(BikeEntry._ID);
-                int makeColumnIndex = cursor.getColumnIndex(BikeEntry.COLUMN_MAKE);
-                int modelColumnIndex = cursor.getColumnIndex(BikeEntry.COLUMN_MODEL);
-                int priceColumnIndex = cursor.getColumnIndex(BikeEntry.COLUMN_PRICE);
-
-                // Run through all the cursor rows
-                while (cursor.moveToNext()) {
-                    int currentID = cursor.getInt(idColumnIndex);
-                    String currentMake = cursor.getString(makeColumnIndex);
-                    String currentModel = cursor.getString(modelColumnIndex);
-                    String currentPrice = cursor.getString(priceColumnIndex);
-                    displayView.append((
-                            " | " + currentID + " | "
-                                    + currentMake + " | "
-                                    + currentModel + " | "
-                                    + "£" + currentPrice
-                                    + " | " + "\n"
-                    ));
-                }
-            } finally {
-                // Always close the cursor when you're done reading from it. This releases all its
-                // resources and makes it invalid.
-                cursor.close();
-            }
-        }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -108,15 +51,13 @@ public class MainActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             // Handles 'insert test data' menu option
             case R.id.insert_test_data:
-                insertTestData();
-                queryDatabase();
+                insertTestBikeData();
                 return true;
         }
         return super.onOptionsItemSelected(item);
     }
 
-    private void insertTestData() {
-        SQLiteDatabase db = bikeDbHelper.getWritableDatabase();
+    private void insertTestBikeData() {
         ContentValues values = new ContentValues();
         values.put(BikeEntry.COLUMN_MAKE, "Cinelli");
         values.put(BikeEntry.COLUMN_MODEL, "Vigorelli");
@@ -126,7 +67,35 @@ public class MainActivity extends AppCompatActivity {
         values.put(BikeEntry.COLUMN_SUPPLIER, "GRUPPO SRL CINELLI");
         values.put(BikeEntry.COLUMN_SUPPLIER_PHONE, "+39 02 95244 1");
 
-        Long newRowID = db.insert(BikeEntry.TABLE_NAME, null, values);
-        Log.v("MainActivity", newRowID.toString());
+        Uri newUri = getContentResolver().insert(BikeEntry.CONTENT_URI, values);
+    }
+
+    @NonNull
+    @Override
+    public android.support.v4.content.Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+        // Define the projections
+        String[] projection = {
+                BikeEntry._ID,
+                BikeEntry.COLUMN_MAKE,
+                BikeEntry.COLUMN_MODEL,
+                BikeEntry.COLUMN_PRICE,
+        };
+        return new CursorLoader(this,
+                BikeEntry.CONTENT_URI,
+                projection,
+                null,
+                null,
+                null
+        );
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor cursor) {
+        bikeCursorAdapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        bikeCursorAdapter.swapCursor(null);
     }
 }
